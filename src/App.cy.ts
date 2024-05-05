2b597dbb2a471e52e2571bbcd82ae9cbac7786e8
OPSTID <90957922+OPSTID@users.noreply.github.com>
2024-05-05 11:13:57 +0900

    テストコード追加

---
+++ src/App.cy.ts
@@ -0,0 +1,17 @@
+import App from './App.vue';
+
+describe('<App />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(App);
+
+    // DL_Sのテスト
+    // RESAS APIからのデータのダウンロードが完了するまで待つ（5秒）
+    cy.wait(5000);
+
+    // ページの1つめの<label>が北海道であることを確認する
+    cy.get('label').first().should('contain', '北海道');
+    // 47都道府県が正しく取得できていること=checkboxが47個あることを確認する
+    cy.get("input[type='checkbox']").its('length').should('eq', 47);
+  });
+});
